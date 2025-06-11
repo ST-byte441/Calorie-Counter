e240feca2cc7bd284c57df8f43d20fdb2ed910f0
ST-byte441 <seungtaekoh5@gmail.com>
2025-06-11 13:34:35 -0700

    feature: homepage css styling basic

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,34 @@
+import React from "react";
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+
+function Home() {
+    const navigate = useNavigate();
+    return(
+        <div className="home">
+            <h1>Solo Project</h1>
+            <button onClick={() => navigate("/about")}>Go to About Page</button>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <button onClick={() => window.history.back()}>Go Back</button>
+        </div>
+    );
+}
+
 
 export default () => {
-    
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('.'));
+app.use(express.static('./dist'));
 
 app.get('/api/quote', async (_req, res) => {
   const database = [
